dc6a6b5a55af31b717af1af0d6f68255347f276a
Sash132 <39384705+Sash132@users.noreply.github.com>
2021-02-11 22:10:11 -0800

    Implement reload fix

--- scripts/script.js
+++ scripts/script.js
@@ -16,6 +16,13 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   cart = JSON.parse(localStorage.getItem('cart'));
   
+  let cartCount = document.getElementById('cart-count');
+  if(localStorage.getItem('cartSize') == null) {
+    localStorage.setItem('cartSize', JSON.stringify(cartSize.textContent));
+  }
+  cartSize = JSON.parse(localStorage.getItem('cartSize'));
+  cartCount.textContent = cartSize;
+  
   for(var prod in productsJSON) {
     let inclusion = cart.includes(productsJSON[prod].id);
     let currProduct = new ProductItem(productsJSON[prod].image, productsJSON[prod].title, productsJSON[prod].price, inclusion, productsJSON[prod].id);
